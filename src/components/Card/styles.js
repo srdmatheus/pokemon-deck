1e38a28d242108001f1e30dcbce3af8bd371179a
Matheus Ribeiro <srdmatheus@gmail.com>
2022-06-22 14:43:58 -0300

    card style finalizado

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -4,8 +4,23 @@ export const CardContent = styled.li`
   display: flex;
   flex-direction: column;
   align-items: center;
+  border-radius: 0.5rem;
+
+  background-color: #fff;
+
+  img {
+    width: 8.5rem;
+    height: auto;
+  }
 
   span {
+    padding: 1rem;
+    width: 100%;
+    border-radius: 0 0 0.5rem 0.5rem;
+
+    text-align: center;
+    font-size: 0.8rem;
     font-weight: bold;
+    background-color: #ccc;
   }
 `
--- src/components/DeckList/styles.js
+++ src/components/DeckList/styles.js
@@ -4,5 +4,5 @@ export const DeckList = styled.ul`
   list-style: none;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  gap: 2rem;
+  gap: 1rem;
 `
