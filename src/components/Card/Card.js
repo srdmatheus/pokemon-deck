fb4f73c605c85bfeb6ba40c5a3ef3a731ec6e72a
Matheus Ribeiro <srdmatheus@gmail.com>
2022-06-22 14:21:51 -0300

    alterando nomes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,51 +1,51 @@
-import { CardPokemon } from './styles'
+import { CardContent } from './styles'
 
 export function Card() {
   const pokemons = [
     {
       id: 1,
       name: 'nome do bixo',
-      picture:
+      image:
         'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
     },
     {
       id: 2,
       name: 'nome do animal',
-      picture:
+      image:
         'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
     },
     {
       id: 3,
       name: 'nome do sujeito',
-      picture:
+      image:
         'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
     },
     {
       id: 4,
       name: 'nome do animal',
-      picture:
+      image:
         'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
     },
     {
       id: 5,
       name: 'nome do animal',
-      picture:
+      image:
         'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
     },
     {
       id: 6,
       name: 'nome do animal',
-      picture:
+      image:
         'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
     }
   ]
 
   return pokemons.map(pokemon => {
     return (
-      <CardPokemon key={pokemon.id}>
-        <img src={pokemon.picture} alt={pokemon.name} />
+      <CardContent key={pokemon.id}>
+        <img src={pokemon.image} alt={pokemon.name} />
         <span>{pokemon.name}</span>
-      </CardPokemon>
+      </CardContent>
     )
   })
 }
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const CardPokemon = styled.li`
+export const CardContent = styled.li`
   display: flex;
   flex-direction: column;
   align-items: center;
