a1968dec58ffa11510a5b0f3f9485c57629fedf1
Matheus Ribeiro <srdmatheus@gmail.com>
2022-06-23 00:54:06 -0300

    limpei bagunça

--- src/components/DeckList/DeckList.jsx
+++ src/components/DeckList/DeckList.jsx
@@ -4,63 +4,21 @@ import { Card } from '../Card/Card'
 import { DeckList } from './styles'
 
 export function Deck() {
-  // const pokemons = [
-  //   {
-  //     id: 1,
-  //     name: 'nome do bixo',
-  //     image:
-  //       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'nome do animal',
-  //     image:
-  //       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'nome do sujeito',
-  //     image:
-  //       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-  //   },
-  //   {
-  //     id: 4,
-  //     name: 'nome do animal',
-  //     image:
-  //       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'nome do animal',
-  //     image:
-  //       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-  //   },
-  //   {
-  //     id: 6,
-  //     name: 'nome do animal',
-  //     image:
-  //       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-  //   }
-  // ]
-
   const [pokemons, setPokemons] = useState([])
 
   useEffect(() => {
     fetch(api)
       .then(response => response.json())
-      .then(data => {
-        console.log(data.results[0])
-        setPokemons(data.results)
-      })
+      .then(data => setPokemons(data.results))
   }, [])
 
   return (
     <DeckList>
-      <div>
+      <>
         {pokemons.map(pokemon => {
           return <Card {...pokemon} />
         })}
-      </div>
+      </>
     </DeckList>
   )
 }
